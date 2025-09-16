7590164f2b8b1b8613a54610cdc16d17079a57a1
KEHINDE ADIGUN <keahnney01@gmail.com>
2025-09-16 23:30:31 +0100

    first commit 51

--- frontend/src/page/LoginAuth.jsx
+++ frontend/src/page/LoginAuth.jsx
@@ -4,8 +4,8 @@ import { auth, googleProvider } from "../lib/firebase";
 import { signInWithPopup } from "firebase/auth";
 import { useAuth } from "../store/AuthReduxContext";
 
-// const BACKEND_URL =  "http://localhost:5000";
-const BACKEND_URL = "https://one0minuteslegal.onrender.com" || "http://localhost:5000";
+const BACKEND_URL =  "http://localhost:5000";
+// const BACKEND_URL = "https://one0minuteslegal.onrender.com" || "http://localhost:5000";
 
 
 const Login = () => {
