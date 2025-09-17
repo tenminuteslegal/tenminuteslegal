93bb9d904cdce2660b6d951f45a8d4a1c845d7a6
KEHINDE ADIGUN <keahnney01@gmail.com>
2025-09-17 18:05:11 +0100

    first commit 55

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,20 @@ const jwt = require("jsonwebtoken");
 const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT_PATH ||
   "./firebaseServiceAccountKey.json");
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    type: process.env.FIREBASE_TYPE, 
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+    universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+  }),
   databaseURL: process.env.VITE_FIREBASE_DATABASE_URL,
 });
 // admin.initializeApp();
