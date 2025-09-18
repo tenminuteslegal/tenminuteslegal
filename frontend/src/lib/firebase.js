435df8a19749ab4eb44a2977fba99afa95593d81
KEHINDE ADIGUN <keahnney01@gmail.com>
2025-09-18 05:33:25 +0100

    new commit 1

--- frontend/src/lib/firebase.js
+++ frontend/src/lib/firebase.js
@@ -1,6 +1,7 @@
 // firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { data } from "react-router-dom";
 
 // 🔑 Your Firebase config (from Firebase Console → Project Settings)
 const firebaseConfig = {
@@ -10,6 +11,7 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_APP_ID,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
 };
 
 // Initialize Firebase App
