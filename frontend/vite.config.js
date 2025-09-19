26d54127d77a704d6a8f6061caef0405a9c7f574
KEHINDE ADIGUN <keahnney01@gmail.com>
2025-09-19 21:53:37 +0100

    new commit 15

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,39 +1,17 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {
-//     outDir: "dist",
-//     rollupOptions: {
-//       input: {
-//         main: "./index.html",
-//       },
-//     },
-//     copyPublicDir: true,
-//   },
-// });
-
-import { resolve } from "path";
 import { defineConfig } from "vite";
-import reactRefresh from "@vitejs/plugin-react-refresh";
-
-const root = resolve(__dirname, "src");
-const outDir = resolve(__dirname, "dist");
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root,
-  plugins: [reactRefresh()],
+  plugins: [react()],
   build: {
-    outDir,
-    emptyOutDir: true,
+    outDir: "dist",
     rollupOptions: {
       input: {
-        main: resolve(root, "index.html"),
-        about: resolve(root, "about", "index.html"),
+        main: "./index.html",
       },
     },
+    copyPublicDir: true,
   },
 });
+
