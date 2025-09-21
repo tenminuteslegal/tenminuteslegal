9d23a4492178d9875f875125de39f40164851546
KEHINDE ADIGUN <keahnney01@gmail.com>
2025-09-21 13:03:58 +0100

    new commit 29

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -1,42 +1,4 @@
-import { supabase } from "./supabaseClient";
-
-export async function createProfileIfNotExists() {
-  const {
-    data: { user },
-    error,
-  } = await supabase.auth.getUser();
-  console.log(user)
-
-  // const {
-  //   data: { Auser },
-
-  // } = await supabase.auth.getUser();
-
-  const { data: profile } = await supabase
-    .from("profiles")
-    .select("role")
-    .eq("id", user.id)
-    .single();
-
-    // console.log(data)
-    console.log(profile)
-  console.log("Role:", profile.role); // "user" or "admin"
-
-  if (error) {
-    console.error("Get user error:", error.message);
-    return;
-  }
-
-  if (user) {
-    await supabase.from("public.profiles").upsert({
-      id: user.id, // UUID from auth.users
-      email: user.email,
-      role: "admin",
-    });
-  }
-  console.log("User profile ensured in database.", user);
-}
-
+/
 export const fetchArticles = async () => {
   const { data, error } = await supabase.from("articles").select("*");
   console.log("Fetched articles:", data);
