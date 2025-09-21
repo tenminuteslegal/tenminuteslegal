b60c29e8c93f6cd3e4929908b645cb65e468ffa0
KEHINDE ADIGUN <keahnney01@gmail.com>
2025-09-21 13:00:48 +0100

    new commit 28

--- frontend/src/lib/supabaseClient.js
+++ frontend/src/lib/supabaseClient.js
@@ -1,11 +1,11 @@
-import { createClient } from "@supabase/supabase-js";
+// import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://eiohrimuzvyvuwntbiba.supabase.co"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVpb2hyaW11enZ5dnV3bnRiaWJhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcxNTQxNTAsImV4cCI6MjA3MjczMDE1MH0.cTROwzK4Unqt0rDShogCsBTbRHbZsBl0MvSHgPICtU0"
-// const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-// const supabaseAnonKey = import.meta.env.SUPABASE_ANON_KEY;
+// const supabaseUrl = "https://eiohrimuzvyvuwntbiba.supabase.co"
+// const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVpb2hyaW11enZ5dnV3bnRiaWJhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcxNTQxNTAsImV4cCI6MjA3MjczMDE1MH0.cTROwzK4Unqt0rDShogCsBTbRHbZsBl0MvSHgPICtU0"
+// // const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+// // const supabaseAnonKey = import.meta.env.SUPABASE_ANON_KEY;
 
-// console.log('env,', import.meta.env.VITE_SUPABASE_URL);
+// // console.log('env,', import.meta.env.VITE_SUPABASE_URL);
 
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+// export const supabase = createClient(supabaseUrl, supabaseAnonKey);
